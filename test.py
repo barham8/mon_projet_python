b236796da11fb8d0efba1d1be7300a759467bb44
barham8 <fallbarham09@gmail.com>
2022-12-18 23:43:07 +0100

    create a new programme

---
+++ test.py
@@ -0,0 +1,8 @@
+nom=input("veuiller entrer votre nom :")
+prenom=input("veuiller votre prenom:")
+annee_naiss=int(input("veuiller entre votre annee de naissance:"))
+print("votre nom est :",nom)
+print("votre prenom est:",prenom)
+print("votre annee de naissance est:",annee_naiss)
+age=annee_courante - annee_naiss
+print("Vous avez ",age,"ans")
